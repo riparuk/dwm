6b5696877e53e336cc5260d8333d31f76af0206a
riparuk <faruqirifa@gmail.com>
2023-04-21 15:50:33 +0700

    add red color

--- config.def.h
+++ config.def.h
@@ -16,10 +16,12 @@ static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
 static const char col_rose[]	    = "#E3242B";
+static const char col_red[]	    = "#8B0000";
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_rose  },
+	[SchemeSel]  = { col_gray4, col_red,  col_rose  },
 };
 
 /* tagging */
